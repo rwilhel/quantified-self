a1cdff19506f99442d058161c558e61f9e1abd3e
rwilhel <rwilhel@gmail.com>
2017-08-27 14:39:53 -0600

    Tests for adding a new food to the foods table on foods.html

--- test/features/viewing-foods-page-test.js
+++ test/features/viewing-foods-page-test.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert
-// const webdriver = require('selenium-webdriver')
+const webdriver = require('selenium-webdriver')
 const until = webdriver.until
-// const test = require('selenium-webdriver/testing')
+const test = require('selenium-webdriver/testing')
 const frontEndLocation = "http://localhost:8080/foods.html"
 
 test.describe('Our test bundle', function() {
@@ -11,7 +11,7 @@ test.describe('Our test bundle', function() {
 })
 
 test.describe('Testing Foods Page', function() {
-	let driver
+	var driver
 	this.timeout(10000)
 
 	test.beforeEach(function () {
@@ -34,4 +34,33 @@ test.describe('Testing Foods Page', function() {
       })
 		})
 	})
+
+	test.it("adds a new food to the foods table", function() {
+		driver.get(`${frontEndLocation}`)
+		driver.wait(until.elementLocated({css: '.add-food-button'}))
+		driver.findElements({css: '.food-name'})
+			.then(function(foods) {
+				initialFoodCount = foods.length
+				driver.findElement({css: 'input[name="food-name"]'}).sendKeys('Chocolate Ice Cream Cone')
+				driver.findElement({css: 'input[name="food-calories"]'}).sendKeys('173')
+				driver.findElement({css: '.add-food-button'}).click()
+				driver.sleep(1000)
+			})
+			.then(function() {
+				driver.findElements({css: '.foods-table'})
+				.then(function(foods) {
+					assert.lengthOf(foods, initialFoodCount + 1)
+				})
+			})
+	})
+
+	// test.it("can delete a food", function() {
+	// 	driver.get(`${frontEndLocation}`)
+	// 	driver.wait(until.elementLocated({css: '.food-name'}))
+	// 	driver.findElement({css: '.trash-can'}).click()
+	// 	driver.findElements({css: '.food-name'})
+	// 		.then(function(foods) {
+	// 			assert.lengthOf(foods, initialFoodCount)
+	// 		})
+	// })
 })
